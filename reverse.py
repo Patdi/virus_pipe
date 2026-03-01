2c388bee436fba5490cd14f2060ee64d951253df
Patdi <25618699+Patdi@users.noreply.github.com>
2026-02-28 23:40:16 -0800

    Add files via upload
    Port to Python3

--- reverse.py
+++ reverse.py
@@ -1,19 +1,28 @@
-#!/usr/bin/env python2.7
-#takes in sequences in tab file format, reverse compliments and prints out sequence name \n then sequence
-import sys 
+#!/usr/bin/env python3
+# Takes in sequences in tab file format, reverse complements, and prints:
+# sequence name \n sequence
+
+import sys
+
 class Gene:
-	def __init__(self, creationid, creationseq):
-		self.id = creationid
-		self.seq = creationseq
-		
-	def reverse_complement(self):
-		return(self.seq[::-1].replace("A", "1").replace("T", "2").replace("G", "3").replace("C", "4").replace("1", "T").replace("2", "A").replace("3", "C").replace("4", "G"))
-		
+    # Supports uppercase + lowercase DNA bases
+    _COMPLEMENT_TABLE = str.maketrans("ATGCatgc", "TACGtacg")
+
+    def __init__(self, creationid, creationseq):
+        self.id = creationid
+        self.seq = creationseq
+
+    def reverse_complement(self):
+        return self.seq[::-1].translate(self._COMPLEMENT_TABLE)
+
 for line in sys.stdin:
-	seq_list = line.strip().split("\t")
-	seq_id = seq_list[0]
-	seq_nu = seq_list[1]
-	new_seq = Gene(seq_id, seq_nu)
-	print seq_id
-	print new_seq.reverse_complement() 
-	
+    seq_list = line.strip().split("\t")
+    if len(seq_list) < 2:
+        continue  # skip malformed/blank lines
+
+    seq_id = seq_list[0]
+    seq_nu = seq_list[1]
+    new_seq = Gene(seq_id, seq_nu)
+
+    print(seq_id)
+    print(new_seq.reverse_complement())
